4a38c4b52979deb4ecca4a15041f044dcf530863
akeela-darkwind <nikiebcpht@gmail.com>
2025-05-07 21:31:44 +0200

    no message

--- src/gencontent.py
+++ src/gencontent.py
@@ -32,7 +32,7 @@ def generate_page(from_path, template_path, dest_path, basepath):
     template = template.replace("{{ Content }}", html)
     template = template.replace('href="/', 'href="' + basepath)
     template = template.replace('src="/', 'src="' + basepath)
-    
+
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
